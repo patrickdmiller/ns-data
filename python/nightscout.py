59d7e39a3b683ce65b8221a965f3718ea8e060af
Patrick Miller <patrickdmiller@gmail.com>
2023-07-21 02:22:14 +0000

    update

--- python/nightscout.py
+++ python/nightscout.py
@@ -18,27 +18,43 @@ class Nightscout:
     self.dbConnection = MongoClient(self.mongo_uri)
     self._entries = None
     self._treatments = None
-  
+    self._devicestatus = None
   
   def get_entries(self, force=False):
     if force or self._entries is None:
       print('!cache miss, fetching from db')
       self._entries = self.find("entries")
+      self._entries['ts'] = pd.to_datetime(self._entries['dateString']) 
+      self._entries = self._entries.sort_values(by=['ts']).reset_index(drop=True)
     return self._entries
   
   def get_treatments(self, force=False):
     if force or self._treatments is None:
       print('!cache miss, fetching from db')
-      self._treatments = self.find("treatments")
+      self._treatments = self.find("treatments")#.sort_values(by=['dateString']).reset_index(drop=True)
+      self._treatments['ts'] = pd.to_datetime(self._treatments['created_at']) 
+      self._treatments = self._treatments.sort_values(by=['ts']).reset_index(drop=True)
     return self._treatments
+  
+  def get_devicestatus(self, force=False):
+    if force or self._devicestatus is None:
+      print('!cache miss, fetching from db')
+      self._devicestatus = self.find("devicestatus")
   
   entries = property(get_entries)
   treatments = property(get_treatments)
-  
+  devicestatus = property(get_devicestatus)
   def align(self, minutes=5):
-    pass
-    
-
+    pe, te = 0,0
+    aligned_entries = []
+    while pe < len(self.entries):
+      entry = self.entries.iloc[pe]
+      if 'sgv' in entry:
+        print(entry['date'], entry['sgv'])
+      
+      pe+=1
+      if pe > 10:
+        break
   #return all rows of collection > more for debugging.
   def find(self, collection="entries", query={}, limit=0):
     cursor = self.dbConnection[DB][collection].find(query).limit(limit)
